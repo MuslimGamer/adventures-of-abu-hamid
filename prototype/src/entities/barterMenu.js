4685dfcc444ae2e85befb7bb4902b3314c69bd7d
NegativeScript <ala2aldeen913@gmail.com>
2018-03-31 20:27:05 +0300

    Player can now buy items

--- prototype/src/entities/barterMenu.js
+++ prototype/src/entities/barterMenu.js
@@ -7,10 +7,31 @@ Crafty.c('BarterMenu', {
 
     setItems: function(items) {
         this.items = items;
-        this.fontSize(config("fontSize")).text(this.display());
+        this.display();
+        this.bind("KeyUp", this.buy);
     },
 
-    display: function(items) {
+    buy: function(e) {
+        var key = e.key;
+        // the index number of the item to be bought
+        var num = key - 48;
+        if (this.items.length >= num) {
+            this.buyItem(this.items[num - 1]);
+            this.display();
+        }
+    },
+
+    buyItem: function(item) {
+        var player = Crafty('Player');
+        player.inventory.push(item)
+        this.items = this.items.filter(i => i !== item)
+    },
+
+    display: function() {
+        this.fontSize(config("fontSize")).text(this.itemsListString());        
+    },
+
+    itemsListString: function(items) {
         var toReturn = "";
         for (var i = 0; i < this.items.length; i++) {
             var item = this.items[i];
