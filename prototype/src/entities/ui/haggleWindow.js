5316c5fa8648ef790541e1b59e40c26292ce07ac
NegativeScript <ala2aldeen913@gmail.com>
2018-04-17 08:56:30 +0300

    Haggling is harder to progress further from the middle

--- prototype/src/entities/ui/haggleWindow.js
+++ prototype/src/entities/ui/haggleWindow.js
@@ -1,3 +1,5 @@
+const BASE_HAGGLE_RATE = 0.05;
+
 Crafty.c('HaggleWindow', {
     init: function () {
         this.requires('InventoryListWindow');
@@ -14,14 +16,23 @@ Crafty.c('HaggleWindow', {
         if (!this.alreadyHaggling && itemIndex >= 0 && itemIndex < this.items.length) {
             this.alreadyHaggling = true;
             var item = this.items[itemIndex];
-            var price = this.priceMap[item.name];
-            this.maxBarValue = price * 2;
+            this.originalPrice = this.priceMap[item.name];
+            this.maxBarValue = this.originalPrice * 2;
+
+            var flipBar, emptyColour, filledColour;
+            if (this.parentWindow.buySellToggle.isPlayerBuying) {
+                emptyColour = "green";
+                filledColour = "red";
+            } else {
+                emptyColour = "red";
+                filledColour = "green";
+            }
 
             this.loadingBar = Crafty.e("Actor, ProgressBar")
                 .attr({w: 100, h: 25, z: 100 })
                 .centerOnScreen()
-                .progressBar(this.maxBarValue, false, "red", "green")
-                .updateBarProgress(price);
+                .progressBar(this.maxBarValue, flipBar, emptyColour, filledColour)
+                .updateBarProgress(this.originalPrice);
             
             this.currentPrice = Crafty.e("Actor, Text2")
                 .size(0, 0)
@@ -36,7 +47,9 @@ Crafty.c('HaggleWindow', {
     haggleOn: function(e) {
         if (e.key == Crafty.keys.H) {
             var currentBarProgress = this.loadingBar._pbFilledFraction * this.maxBarValue;
-            this.loadingBar.updateBarProgress(currentBarProgress + this.maxBarValue * 0.05);
+            var haggleDirection = this.parentWindow.buySellToggle.isPlayerBuying ? -1 : 1;
+            var haggleRate = this.getHaggleRate(haggleDirection);
+            this.loadingBar.updateBarProgress(currentBarProgress + this.maxBarValue * haggleRate * haggleDirection);
             this.updatePrice();
         }
     },
@@ -45,18 +58,29 @@ Crafty.c('HaggleWindow', {
         var now = Date.now();
         if (now - this.lastHaggleTime > 500) {
             var currentBarProgress = this.loadingBar._pbFilledFraction * this.maxBarValue;
-            this.loadingBar.updateBarProgress(currentBarProgress - this.maxBarValue * 0.05);
+            var haggleDirection = this.parentWindow.buySellToggle.isPlayerBuying ? 1 : -1;
+            var haggleRate = this.getHaggleRate(haggleDirection);
+            this.loadingBar.updateBarProgress(currentBarProgress + this.maxBarValue * haggleRate * haggleDirection);
             this.updatePrice();
             this.lastHaggleTime = Date.now();
         }
     },
 
+    getHaggleRate: function(haggleDirection) {
+        var fraction = this.loadingBar._pbFilledFraction;
+        if (haggleDirection == 1) {
+            fraction = 1 - fraction;
+        }
+        return fraction * BASE_HAGGLE_RATE
+    },
+ 
     updatePrice: function() {
         var newPrice = Math.round(this.loadingBar._pbFilledFraction * this.maxBarValue);
         this.currentPrice.text(newPrice.toString());
     },
 
     setParentWindow: function(parentWindow) {
+        this.parentWindow = parentWindow;
         this.items = parentWindow.items;
         this.priceMap = parentWindow.priceMap;
         this.favouriteItem = parentWindow.favouriteItem;
